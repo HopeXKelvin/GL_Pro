06e8734391e992c38845888579d804e395474606
kelvin <hope_kelvin@163.com>
2017-04-11 08:50:38 +0800

    添加three.js相关的库引用进去

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 var path = require('path');
 
 module.exports = {
-  entry : './public/scripts/index.js',
+  entry : {
+    index : './public/scripts/index',
+    drum_display : ['./public/scripts/lib/three','./public/scripts/lib/MTLLoader','./public/scripts/lib/OBJLoader','./public/scripts/drum_display']
+  },
   output : {
-    filename : 'bundle.js',
+    filename : '[name].js',
     path : path.resolve(__dirname,'dist/scripts')
+  },
+  module : {
+    rules : [
+      {
+        test : /\.less$/,
+        use : [
+          {loader : 'style-loader'},// create style nodes from JS strings
+          {loader : 'css-loader'},// 把CSS转化成CommonJS(问好脸,css还能转成 commonjs?)
+          {loader : 'less-loader'}// 把Less编译成CSS
+        ]
+      }
+    ]
   }
 };
